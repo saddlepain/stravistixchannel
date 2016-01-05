8188176f003c5f0fd49248bb4fb2609e2f853afd
Travis CI <travis@travis-ci.org>
2016-01-05 10:43:50 +0000

    Deployment commit on this development channel from a github.com/saddlepain/stravistixchannel.git:develop change

---
+++ options/app/Config.js
@@ -0,0 +1,12 @@
+/**
+ * Declaring Angular App Properties
+ */
+var routeMap = {
+    commonSettingsRoute: '/commonSettings',
+    healthSettingsRoute: '/healthSettings',
+    zonesSettingsRoute: '/zonesSettings',
+    releaseNotesRoute: '/releaseNotes',
+    aboutRoute: '/about',
+    donateRoute: '/donate',
+    shareRoute: '/share'
+};
